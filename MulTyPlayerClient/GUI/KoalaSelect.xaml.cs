58f9f8d46ae52e09802d6b0daa5bd337e1c3d898
ElusiveFluffy <Freya8822@hotmail.com>
2023-01-22 19:07:04 +1030

    Cleaned up xaml code and added spotlight functionallity

--- MulTyPlayerClient/GUI/KoalaSelect.xaml.cs
+++ MulTyPlayerClient/GUI/KoalaSelect.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,51 @@ namespace MulTyPlayerClient.GUI
     /// </summary>
     public partial class KoalaSelect : Window
     {
+        public static KoalaImages KoalasClass = new KoalaImages();
+
         public KoalaSelect()
         {
             InitializeComponent();
+        }
+
+        private void Boonie_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KoalasClass.Boonie = (bool)e.NewValue ? @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Light/Boonie.png" : @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Dark/Boonie.jpg";
+        }
+
+        private void Mim_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KoalasClass.Mim = (bool)e.NewValue ? @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Light/Mim.png" : @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Dark/Mim.jpg";
+        }
+
+        private void Gummy_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KoalasClass.Gummy = (bool)e.NewValue ? @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Light/Gummy.png" : @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Dark/Gummy.jpg";
+        }
+
+        private void Snugs_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KoalasClass.Snugs = (bool)e.NewValue ? @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Light/Snugs.png" : @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Dark/Snugs.jpg";
+        }
+
+        private void Katie_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KoalasClass.Katie = (bool)e.NewValue ? @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Light/Katie.png" : @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Dark/Katie.jpg";
+        }
+
+        private void Kiki_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KoalasClass.Kiki = (bool)e.NewValue ? @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Light/Kiki.png" : @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Dark/Kiki.jpg";
+        }
+
+        private void Elizabeth_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KoalasClass.Elizabeth = (bool)e.NewValue ? @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Light/Elizabeth.png" : @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Dark/Elizabeth.jpg";
+        }
+
+        private void Dubbo_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KoalasClass.Dubbo = (bool)e.NewValue ? @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Light/Dubbo.png" : @"pack://siteoforigin:,,,/GUI/KoalaSelectionAssets/Dark/Dubbo.jpg";
         }
     }
 }
