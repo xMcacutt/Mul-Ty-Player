9d2739a0d3a59643a6a0ed7cd5c7e15e19ce9d6f
xMcacutt <matthew.cacutt@gmail.com>
2023-04-04 21:25:26 +0100

    Crates after 300 opal TE fix attempt 1

--- MulTyPlayerClient/Classes/Sync/TE/TEHandler.cs
+++ MulTyPlayerClient/Classes/Sync/TE/TEHandler.cs
@@ -35,6 +35,44 @@ namespace MulTyPlayerClient
             return (previousState < 3 && currentState > 3);
         }
 
+        public override void HandleClientUpdate(int iLive, int iSave, int level)
+        {
+            GlobalObjectData[level][iLive] = (byte)CheckState;
+            SaveSync.Save(iSave, level);
+            if (level != Client.HLevel.CurrentLevelId) return;
+            if (iSave == 0) { Client.HSync.SyncObjects["Crate"].GlobalObjectData[level] = new byte[CrateHandler.CratesPerLevel[level]]; }
+            LiveSync.Collect(iLive);
+        }
+
+        public override void CheckObserverChanged()
+        {
+            ObserverState = ReadObserver(CounterAddress, CounterByteLength);
+            if (PreviousObserverState == ObserverState || ObserverState == 0) return;
+            PreviousObserverState = ObserverState;
+            CurrentObjectData = LiveSync.ReadData();
+            int iSave;
+            for (int iLive = 0; iLive < CurrentObjectData.Length; iLive++)
+            {
+                if (CheckObserverCondition(PreviousObjectData[iLive], CurrentObjectData[iLive]))
+                {
+                    iSave = iLive;
+                    PreviousObjectData[iLive] = CurrentObjectData[iLive] = WriteState;
+                    if (SeparateID)
+                    {
+                        int address = LiveObjectAddress + (iLive * LiveSync.ObjectLength) + IDOffset;
+                        ProcessHandler.TryRead(address, out iSave, false);
+                    }
+                    if (GlobalObjectData[Client.HLevel.CurrentLevelId][iLive] != CheckState)
+                    {
+                        //BasicIoC.LoggerInstance.Write(Name + " number " + iLive + " collected.");
+                        GlobalObjectData[Client.HLevel.CurrentLevelId][iLive] = (byte)CheckState;
+                        if (iSave == 0) { Client.HSync.SyncObjects["Crate"].GlobalObjectData[Client.HLevel.CurrentLevelId] = new byte[CrateHandler.CratesPerLevel[Client.HLevel.CurrentLevelId]]; }
+                        Client.HSync.SendDataToServer(iLive, iSave, Client.HLevel.CurrentLevelId, Name);
+                    }
+                }
+            }
+        }
+
         public  override void SetMemAddrs()
         {
             CounterAddress = PointerCalculations.GetPointerAddress(0x00288730, new int[] { 0xD });
