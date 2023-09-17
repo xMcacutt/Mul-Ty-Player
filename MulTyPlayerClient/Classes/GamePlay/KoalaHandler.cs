bb44cccd332d83aa1fd66b93367965ff7b7b2afa
Connor Holmes <connorholmes.419@gmail.com>
2023-09-18 02:41:20 +0800

    Added rudimentary transform interpolation. Untested, unconfirmed.

--- MulTyPlayerClient/Classes/GamePlay/KoalaHandler.cs
+++ MulTyPlayerClient/Classes/GamePlay/KoalaHandler.cs
@@ -1,52 +1,51 @@
-﻿using MulTyPlayerClient.GUI;
+﻿using MulTyPlayerClient.Classes.Networking;
+using MulTyPlayerClient.GUI;
 using MulTyPlayerClient.GUI.Models;
 using Riptide;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MulTyPlayerClient
 {
     internal class KoalaHandler
     {
+        public KoalaTransformPtrs[] koalaTransforms;
+        
         static GameStateHandler HGameState => Client.HGameState;
         static LevelHandler HLevel => Client.HLevel;
 
-        public KoalaTransformPtrs[] koalaTransforms;
+        static InterpolationMode interpolationMode = InterpolationMode.None;
+        static bool readyToWriteTransformData = false;
 
         public struct KoalaTransformPtrs
         {
-            public int X;
-            public int Y;
-            public int Z;
-            public int Pitch;
-            public int Yaw;
-            public int Roll;
-            public int Collision;
-            public int Visibility;
+            public int X, Y, Z, Pitch, Yaw, Roll, Collision, Visibility;
         }
 
-        struct KoalaTransform
+        struct TransformSnapshot
         {
-            public float X, Y, Z, Pitch, Yaw, Roll;
+            public float[] Transform;
+            public DateTime Timestamp;
 
-            public KoalaTransform(float[] f)
+            public TransformSnapshot(float[] kt, DateTime dt)
             {
-                X = f[0];
-                Y = f[1];
-                Z = f[2];
-                Pitch = f[3];
-                Yaw = f[4];
-                Roll = f[5];
+                Transform = kt;
+                Timestamp = dt;
             }
+        }
+
+        struct TransformSnapshotPair
+        {
+            public TransformSnapshot Old;
+            public TransformSnapshot New;
         }
 
         static int _bTimeAttackAddress = 0x28AB84;
         static int _baseKoalaAddress;
 
         //                      koala id
-        private static Dictionary<int, KoalaTransform> playerKoalaTransforms = new();
-        private static bool interpolateKoalaPositions = false;
-        private static bool interpolateKoalaRotations = false;
+        private static Dictionary<int, TransformSnapshotPair> playerKoalaTransforms = new();
 
         public KoalaHandler()
         {
@@ -66,6 +65,7 @@ namespace MulTyPlayerClient
             Client.HKoala.SetCoordAddrs();
             Client.HSync.SetMemAddrs();
             Client.HSync.RequestSync();
+            playerKoalaTransforms.Add((int)k, new TransformSnapshotPair());
         }
 
         public void CreateKoalaAddressArray()
@@ -84,6 +84,7 @@ namespace MulTyPlayerClient
 
         public void SetCoordAddrs()
         {
+            readyToWriteTransformData = false;
             //KOALAS ARE STRUCTURED DIFFERENTLY IN STUMP AND SNOW SO MODIFIER AND OFFSET ARE NECESSARY
             bool levelHasKoalas = Levels.GetLevelData(Client.HLevel.CurrentLevelId).HasKoalas;
             int modifier = levelHasKoalas ? 2 : 1;
@@ -111,6 +112,8 @@ namespace MulTyPlayerClient
 
             if (!SettingsHandler.Settings.DoKoalaCollision)
                 RemoveCollision();
+
+            readyToWriteTransformData = true;
         }
 
         public void RemoveCollision()
@@ -166,7 +169,10 @@ namespace MulTyPlayerClient
             //Return if player is on the main menu or loading screen,
             //No need to set coords
             if (HGameState.IsAtMainMenuOrLoading())
+            {
+                readyToWriteTransformData = false;
                 return;
+            }
 
             //If failed to get this clients player, or received our own coordinates, return
             if (!PlayerHandler.Players.TryGetValue(Client._client.Id, out Player p) ||
@@ -178,9 +184,69 @@ namespace MulTyPlayerClient
                 return;
 
             float[] transform = message.GetFloats();
-            playerKoalaTransforms[koalaID] = new(transform);
 
-            WriteCoordinateData(koalaID, transform);            
+            if (interpolationMode == InterpolationMode.None)
+            {
+                //Do nothing, write coords as received
+                WriteCoordinateData(koalaID, transform);
+            }
+            else
+            {
+                //Writes to the transform array in-place
+                InterpolateKoalaTransforms(koalaID, transform); 
+            }            
+        }
+
+        private static void InterpolateKoalaTransforms(int koalaID, float[] incomingTransform)
+        {
+            TransformSnapshot newTS = new(incomingTransform, DateTime.Now);
+            var pair = playerKoalaTransforms[koalaID];
+            pair.Old = pair.New;
+            pair.New = newTS;
+
+            switch (interpolationMode)
+            {
+                case InterpolationMode.None:
+                    {
+                        //Do nothing, write coords as received
+                        //Performant, reliable, but looks stuttery
+                        return;
+                    }
+                case InterpolationMode.Interpolate:
+                    {
+                        // Lerp between the second last transform and the last transform received, based on how much time has passed.
+                        // Stops once the last transform has been reached with no further packets received
+                        // Looks smoother, but costs cpu, and adds latency to movement (~20-30ms)
+                        Interpolation.LerpFloatsNonAllocClamped(
+                            pair.Old.Transform, pair.Old.Timestamp,
+                            pair.New.Transform, pair.New.Timestamp,
+                            incomingTransform, 6);
+                        break;
+                    }
+                case InterpolationMode.Extrapolate:
+                    {
+                        // Lerp between the second last transform and the last transform received, based on how much time has passed.
+                        // Does not stop upon reaching the latest transform, attempts to extrapolate further
+                        // Looks smoother, but costs cpu, and adds latency to movement (~20-30ms)
+                        // May be better than interpolate for players with shaky connections
+                        Interpolation.LerpFloatsNonAlloc(
+                            pair.Old.Transform, pair.Old.Timestamp,
+                            pair.New.Transform, pair.New.Timestamp,
+                            incomingTransform, 6);
+                        break;
+                    }
+                case InterpolationMode.Predictive:
+                    {
+                        // Predict and extrapolate the koalas movement based on the last two transforms received
+                        // Smoother than none, no latency, but costs cpu
+                        // Could be unpredictable/innaccurate with shaky connections
+                        Interpolation.PredictFloatsNonAlloc(
+                            pair.Old.Transform, pair.Old.Timestamp,
+                            pair.New.Transform, pair.New.Timestamp,
+                            incomingTransform, 6);
+                        break;
+                    }
+            }
         }
 
         private static void WriteCoordinateData(int koalaID, float[] coordinates)
--- MulTyPlayerClient/Classes/Networking/Interpolation.cs
+++ MulTyPlayerClient/Classes/Networking/Interpolation.cs
@@ -8,33 +8,71 @@ namespace MulTyPlayerClient.Classes.Networking
         //bTime should be AFTER aTime, otherwise it will produce the opposite result
         public static float PredictFloat(float a, DateTime aTime, float b, DateTime bTime)
         {
-            float vel = (b - a) / bTime.Subtract(aTime).Milliseconds;
-            return b + (vel * DateTime.Now.Subtract(bTime).Milliseconds);
+            double vel = (b - a) / bTime.Subtract(aTime).TotalMilliseconds;
+            return (float)(b + (vel * DateTime.Now.Subtract(bTime).TotalMilliseconds));
         }
 
-        public static float[] LerpFloats(float[] f1, float[] f2, float t, int size)
+        public static void PredictFloatsNonAlloc(float[] f1, DateTime aTime, float[] f2, DateTime bTime, float[] result, int size)
         {
-            float[] result = new float[size];
+            double timeSinceB = DateTime.Now.Subtract(bTime).TotalMilliseconds;
+            double timeBetween = bTime.Subtract(aTime).TotalMilliseconds;
+            float mTime = (float)(timeSinceB / timeBetween) + 1f;
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = f2[i] + mTime * (f2[i] - f1[i]);
+            }
+        }
+
+        public static void LerpFloatsNonAlloc(float[] f1, DateTime dt1, float[] f2, DateTime dt2, float[] result, int size)
+        {
+            double dtGap = (dt2 - dt1).TotalMilliseconds;
+            double dtNow = (DateTime.Now - dt2).TotalMilliseconds;
+            float t = (float)(dtNow / dtGap);
+            if (t <= 0.0f)
+            {
+                f1.CopyTo(result, 0);
+                return;
+            }
             for (int i = 0; i < size; i++)
             {
                 result[i] = Lerpf(f1[i], f2[i], t);
             }
-            return result;
         }
 
-        public static void LerpFloatsNonAlloc(float[] f1, float[] f2, float t, float[] result, int size)
+        public static void LerpFloatsNonAllocClamped(float[] f1, DateTime dt1, float[] f2, DateTime dt2, float[] result, int size)
         {
+            double dtGap = (dt2 - dt1).TotalMilliseconds;
+            double dtNow = (DateTime.Now - dt2).TotalMilliseconds;
+            float t = (float)(dtNow / dtGap);
+
+            if (t <= 0.0f)
+            {
+                f1.CopyTo(result, 0);
+                return;
+            }
+            else if (t >= 1.0f)
+            {
+                f2.CopyTo(result, 0);
+                return;
+            }
+
             for (int i = 0; i < size; i++)
             {
                 result[i] = Lerpf(f1[i], f2[i], t);
             }
         }
 
-        //Use a value of t between 0 -> 1 to lerp from a -> b
-        //This is not clamped so using other values will result in extrapolation
-        private static float Lerpf(float a, float b, float t)
+        private static float Lerpf(float f1, float f2, float t)
         {
-            return a * t + (1 - t) * b;
+            return f1 * (1 - t) + f2 * t;
         }
+    }
+
+    public enum InterpolationMode
+    {
+        None,
+        Interpolate,
+        Extrapolate,
+        Predictive
     }
 }
