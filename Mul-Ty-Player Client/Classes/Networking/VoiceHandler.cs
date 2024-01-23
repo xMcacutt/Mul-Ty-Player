05bfb20622ee4f38fa53c196328696edaff6dc92
xMcacutt <matthew.cacutt@gmail.com>
2024-01-23 08:00:44 +0000

    lower rate test

--- Mul-Ty-Player Client/Classes/Networking/VoiceHandler.cs
+++ Mul-Ty-Player Client/Classes/Networking/VoiceHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
-using System.Timers;
 using MulTyPlayer;
 using NAudio.Wave;
 using Riptide;
@@ -12,12 +11,10 @@ public class VoiceHandler
 {
     private static WaveInEvent _waveIn;
     private const float Range = 1000f;
-    private const int SampleRate = 11025;
-    private const int BitDepth = 16;
-    private const int BufferMillis = 5;
+    private const int SampleRate = 8000;
+    private const int BitDepth = 8;
+    private const int BufferMillis = 50;
     private static Dictionary<ushort, (IWavePlayer, BufferedWaveProvider)> _voices;
-    private static Timer _sendTimer;
-    private static List<byte> _voiceBuffer = new List<byte>();
 
     [MessageHandler((ushort)MessageID.Voice)]
     private static void ReceiveVoiceData(Message message)
@@ -41,7 +38,6 @@ public class VoiceHandler
         _voices.Add(clientId, (new DirectSoundOut(), new BufferedWaveProvider(new WaveFormat(SampleRate, BitDepth, 1))));
         _voices[clientId].Item1.Init(_voices[clientId].Item2);
         _voices[clientId].Item1.Play();
-        Console.WriteLine($"Playing voice {clientId}");
     }
 
     public static void RemoveVoice(ushort clientId)
@@ -66,31 +62,10 @@ public class VoiceHandler
             WaveFormat = new WaveFormat(SampleRate, BitDepth, 1),
             BufferMilliseconds = BufferMillis 
         };
-        _sendTimer = new Timer(10); // Adjust the interval based on your needs
-        _sendTimer.Elapsed += SendBufferedData;
-        _sendTimer.AutoReset = true;
-        _sendTimer.Start();
         _waveIn.DataAvailable += WaveIn_DataAvailable;
         _waveIn.StartRecording();
     }
-
-    private static void SendBufferedData(object sender, ElapsedEventArgs e)
-    {
-        try
-        {
-            if (_voiceBuffer.Count == 0)
-                return;
-            var message = Message.Create(MessageSendMode.Unreliable, MessageID.Voice);
-            message.AddBytes(_voiceBuffer.ToArray());
-            Client._client.Send(message);
-            _voiceBuffer.Clear();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-    }
-
+    
     public static void StopCaptureVoice()
     {
         if (_waveIn == null) return;
@@ -103,7 +78,9 @@ public class VoiceHandler
     {
         try
         {
-            _voiceBuffer.AddRange(e.Buffer);
+            var message = Message.Create(MessageSendMode.Unreliable, MessageID.Voice);
+            message.AddBytes(e.Buffer);
+            Client._client.Send(message);
         }
         catch (Exception ex)
         {
