9da4eecd18e7dbf95b1348e970178158f4946fdf
xMcacutt <matthew.cacutt@gmail.com>
2024-07-01 12:13:37 +0100

    fixed again

--- Mul-Ty-Player Client/Classes/Sync/GameInfoHandler.cs
+++ Mul-Ty-Player Client/Classes/Sync/GameInfoHandler.cs
@@ -13,7 +13,7 @@ public class GameInfoHandler
     
     public void ActivateGameInfoScreen(int level)
     {
-        if (levelsActive[level] || level < 0 || level > 23)
+        if (level < 0 || level > 23 || levelsActive[level] )
             return;
         ProcessHandler.WriteData(SyncHandler.SaveDataBaseAddress + 0xB0 * level, new byte[] { 0x1 });
         levelsActive[level] = true;
