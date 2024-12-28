1848699b84d7ea43ecc65830a8391becf8cff599
xMcacutt <matthew.cacutt@gmail.com>
2024-12-28 19:39:13 +0000

    v4.9.11

--- Mul-Ty-Player Client/Classes/Sync/Mushrooms/LiveMushroomSyncer.cs
+++ Mul-Ty-Player Client/Classes/Sync/Mushrooms/LiveMushroomSyncer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Diagnostics.Tracing.AutomatedAnalysis;
 
 namespace MulTyPlayerClient;
 
@@ -37,29 +38,22 @@ internal class LiveMushroomSyncer : LiveDataSyncer
             // ACTIVATE THE TRIGGER AND MAKE THE TELEPORTER VISIBLE
             Client.HSync.HTrigger.SetTriggerActivity(triggerIndex, true);
             var byteToWrite = state ? (byte)0x1 : (byte)0x0;
-            ProcessHandler.WriteData(triggerEntryAddr + 0xC8, new byte[] { 0x0, byteToWrite, byteToWrite });
+            ProcessHandler.WriteData(triggerEntryAddr + 0xC8, new byte[] { 0x0, byteToWrite });
         }
     }
 
     public static bool GetMushroomState()
     {
+        ProcessHandler.TryRead(0x26D6FC, out int tpCount, true, "readTPCount");
+        ProcessHandler.TryRead(0x26D700, out int tpAddr, true, "readTPAddr");
         var countActive = 0;
-        var triggerSphereCount = Client.HSync.HTrigger.GetTriggerSphereCount();
-        for (var triggerIndex = 0; triggerIndex < triggerSphereCount; triggerIndex++)
+        for (var tpIndex = 0; tpIndex < tpCount; tpIndex++)
         {
-            // FIND TRIGGERS WHERE ENTRY AND EXIT EVENTS TARGET THE SAME OBJECT
-            var triggerEntryAddr = Client.HSync.HTrigger.GetTriggerEnterTargetAddress(triggerIndex);
-            var triggerExitAddr = Client.HSync.HTrigger.GetTriggerExitTargetAddress(triggerIndex);
-            if (triggerEntryAddr != triggerExitAddr)
-                continue;
-            // CHECK IF ENTRY AND EXIT EVENTS ARE TARGETING A TELEPORTER 
-            ProcessHandler.TryRead(triggerEntryAddr + 4, out int indicatorAddr, false, "Find Teleporter Trigger");
-            if (indicatorAddr != TyProcess.BaseAddress + 0x26D68C)
-                continue;
-            // GET TELEPORTER STATE
-            ProcessHandler.TryRead(triggerEntryAddr + 0xC9, out bool active, false, "ReadState");
-            if (active)
+            ProcessHandler.TryRead(tpAddr + 0xC8, out bool state1, false, "readTPAddr");
+            ProcessHandler.TryRead(tpAddr + 0xC9, out bool state2, false, "readTPAddr");
+            if (!state1 && state2)
                 countActive++;
+            ProcessHandler.TryRead(tpAddr + 0x34, out tpAddr, false, "nextTPAddr");
         }
         return countActive > 1;
     }
